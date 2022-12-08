982e2acbc151bc1a370caafbd586292aa52e8ea2
ULTIMATE-Mystery <quanghayhay1801@gmail.com>
2022-12-09 03:05:28 +0700

    Update...

---
+++ src/Handler/Server.java
@@ -0,0 +1,45 @@
+package Handler;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+    public static Vector<Vector<String>> users;
+    private static final int serverPort = 5000;
+    public static Vector<Client> active;
+
+    public static boolean addUser(String username, String password) {
+        for (Vector<String> temp : users) {
+            if (temp.get(0).equals(username)) {
+                return false;
+            }
+        }
+        Vector<String> user = new Vector<>();
+        user.add(username);
+        user.add(password);
+        users.add(user);
+        return true;
+    }
+
+    public static void init() {
+        try {
+            users = new Vector<Vector<String>>();
+            active = new Vector<Client>();
+            ServerSocket ss = new ServerSocket(serverPort);
+            do {
+                System.out.println("Waiting for a client!");
+                Socket s = ss.accept();
+                ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+                ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+                System.out.println("Welcome client!");
+                Client c = new Client(s, "", ois, oos);
+                c.start();
+            } while (true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
